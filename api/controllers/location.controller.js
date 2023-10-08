d7780eceeb93b11af0e9670c9c386c8f11b64a8f
Clintafton2003 <Clintafton@gmail.com>
2023-10-08 16:56:24 +0700

    fix format

--- api/controllers/location.controller.js
+++ api/controllers/location.controller.js
@@ -1,32 +1,32 @@
 const locationSchema = require("../models/location.model");
 
 const locationController = {
-  getLocations: async (req, res) => {
-    try {
-      const location = await locationSchema.find();
-      res.status(200).json(location);
-    } catch (error) {
-      res.status(500).json({ message: error.message });
-    }
-  },
-  createLocations: async (req, res) => {
-    try {
-      const location = new locationSchema(req.body);
-      const saveLocation = await location.save();
-      res.status(200).json(saveLocation);
-    } catch (error) {
-      res.status(500).json({ message: error.message });
-    }
-  },
-  deleteLocations: async (req, res) => {
-    try {
-      const location = locationSchema.find();
-      const removeLocation = await location.deleteMany();
-      res.status(200).json(removeLocation);
-    } catch (error) {
-      res.status(500).json({ message: error.message });
-    }
-  },
+    getLocations: async (req, res) => {
+        try {
+            const location = await locationSchema.find();
+            res.status(200).json(location);
+        } catch (error) {
+            res.status(500).json({ message: error.message });
+        }
+    },
+    createLocations: async (req, res) => {
+        try {
+            const location = new locationSchema(req.body);
+            const saveLocation = await location.save();
+            res.status(200).json(saveLocation);
+        } catch (error) {
+            res.status(500).json({ message: error.message });
+        }
+    },
+    deleteLocations: async (req, res) => {
+        try {
+            const location = locationSchema.find();
+            const removeLocation = await location.deleteMany();
+            res.status(200).json(removeLocation);
+        } catch (error) {
+            res.status(500).json({ message: error.message });
+        }
+    },
 };
 
 module.exports = locationController;
